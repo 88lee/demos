096dd2db8f85cc98adc233bd99d8119b96971c2d
李渊 <liyuanh@yonyou.com>
2018-11-24 02:28:01 +0800

    freemarker基本语法

---
+++ xcEdu/xcEduService01/test-freemarker/src/main/java/com/xuecheng/test/freemarker/FreemarkerTestApplication.java
@@ -0,0 +1,17 @@
+package com.xuecheng.test.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LiYuan
+ * Created on 2018/11/24.
+ */
+@SpringBootApplication
+public class FreemarkerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreemarkerTestApplication.class, args);
+    }
+
+}
---
+++ xcEdu/xcEduService01/test-freemarker/src/main/java/com/xuecheng/test/freemarker/controller/FreemarkerController.java
@@ -0,0 +1,61 @@
+package com.xuecheng.test.freemarker.controller;
+
+import com.xuecheng.test.freemarker.model.Student;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author LiYuan
+ * Created on 2018/11/24.
+ */
+@RequestMapping("/freemarker")
+@Controller //不要使用RestController，否则会相应json数据即模板名称
+public class FreemarkerController {
+
+    //    @Autowired
+    //    RestTemplate restTemplate;
+
+    @RequestMapping("/test1")
+    public String freemarker(Map<String, Object> model) {
+        //向数据模型放数据
+        model.put("name", "黑马程序员");
+        Student stu1 = new Student();
+        stu1.setName("小明");
+        stu1.setAge(18);
+        stu1.setMoney(1000.86f);
+        stu1.setBirthday(new Date());
+        Student stu2 = new Student();
+        stu2.setName("小红");
+        stu2.setMoney(200.1f);
+        stu2.setAge(19);
+        stu2.setBirthday(new Date());
+        List<Student> friends = new ArrayList<>();
+        friends.add(stu1);
+        stu2.setFriends(friends);
+        stu2.setBestFriend(stu1);
+        List<Student> stus = new ArrayList<>();
+        stus.add(stu1);
+        stus.add(stu2);
+        //向数据模型放数据
+        model.put("stus", stus);
+        //准备map数据
+        HashMap<String, Student> stuMap = new HashMap<>();
+        stuMap.put("stu1", stu1);
+        stuMap.put("stu2", stu2);
+        //向数据模型放数据
+        model.put("stu1", stu1);
+        //向数据模型放数据
+        model.put("stuMap", stuMap);
+        model.put("point", 999999999);
+        //返回模板文件名称,基于resources/templates路径
+        return "test1";
+    }
+
+}
