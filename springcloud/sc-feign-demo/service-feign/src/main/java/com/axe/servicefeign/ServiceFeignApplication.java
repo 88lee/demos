3555d7a512e65ade63e1ffc109f6b99749feee5e
李渊 <liyuanh@yonyou.com>
2019-05-19 03:45:52 +0800

    Feign + HystrixDashboard 配置(依赖,actuator属性,注解)

--- springcloud/sc-feign-demo/service-feign/src/main/java/com/axe/servicefeign/ServiceFeignApplication.java
+++ springcloud/sc-feign-demo/service-feign/src/main/java/com/axe/servicefeign/ServiceFeignApplication.java
@@ -2,6 +2,8 @@ package com.axe.servicefeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -12,7 +14,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * 而EnableEurekaClient注解因为没有再像之前的版本一样引入EnableDiscoveryClient而失效
  *
  * Feign集成了Ribbon技术，所以也支持负载均衡（默认轮询，如需要替换可以在将自己需要的规则添加到容器中）
+ *
+ * 添加依赖后,还需要配置 actuator 相关属性,再配合EnableHystrixDashboard 和 EnableCircuitBreaker 注解,即可开启 HystrixDashboard
  */
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @EnableFeignClients
 @SpringBootApplication
 public class ServiceFeignApplication {
